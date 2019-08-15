7dd4511bf256da039f27df3c06e3440d3698f382
Joseph Corasaniti <joecorso37@gmail.com>
2019-08-15 11:30:25 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Todoey
+//
+//  Created by Joe Corasaniti on 8/15/19.
+//  Copyright © 2019 Joseph Corasaniti. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Item {
+    var title:String = ""
+    var done:Bool = false
+}
